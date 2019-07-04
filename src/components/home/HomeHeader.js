738df998c56415a3db5ef7c19351364452a0e548
Martin Litre <martinlitre@mac.com>
2019-07-04 10:07:00 +0200

    Fixed typos

--- src/components/home/HomeHeader.js
+++ src/components/home/HomeHeader.js
@@ -18,7 +18,7 @@ const HomeHeader = () => (
 
             <section className="grid-12 gutter-row-20 gutter-36-ns mt10 mt20-ns mt25-l miw-100 miw-auto-ns home-main-box-margin-ns z-999">
                 <HomeHeaderBox
-                    to="//biodynamo/doc/user_guide/docs/"
+                    to="/biodynamo/doc/user_guide/docs/"
                     title="User Guide"
                     icon="book1"
                     color="tutorial-green"
