06c045acceaa318e897581a1bb24ed4125549448
Lukas Johannes Breitwieser <lukas.johannes.breitwieser@cern.ch>
2019-07-11 16:14:28 +0200

    WIP Use hard coded search results

--- src/components/common/search/Search.js
+++ src/components/common/search/Search.js
@@ -68,7 +68,7 @@ class Results extends React.Component {
         //     value: this.props.currentRefinement,
         // }
         this.state = {
-            value: "foobar",
+            value: "",
         }
 
         this.onChange = this.onChange.bind(this)
@@ -115,7 +115,7 @@ class Results extends React.Component {
         // // console.log(JSON.stringify(hit))
         // console.log(new Error().stack);
         // return <HitTemplate hit={hit} />
-        return (<div>foo result </div>)
+        return (<div>{hit.title} </div>)
     }
 
     renderSectionTitle({ index }) {
@@ -143,6 +143,7 @@ class Results extends React.Component {
     }
 
     getSectionSuggestions(section) {
+        console.log("getSectionSuggestions")
         return section.hits
     }
 
@@ -150,7 +151,9 @@ class Results extends React.Component {
         console.log("asdf")
         // Don't show sections with no results
         // const hits = this.props.hits.filter(hit => hit.hits && hit.hits.length !== 0)
-        const hits = [{"text":"hello"}, {"text":"world"}]
+        // const hits = [{"text":"hello"}, {"text":"world"}]
+        const hits = [{"slug":"migrating-to-digitalocean","title":"How we migrated 30,000 blogs & 400,000 users across an ocean with zero downtime","html":"<p>The first step was to create an inventory of what was currently running on our servers.</p><p>Every system administrator knows what software is running in their datacenter, but I don't know anyone who could replicate the configuration of multiple servers by heart. Including me. So we needed to gather all of the important (and maybe unimportant) information about our servers.</p><p>We introduced a configuration management system to keep track of installed software packages, firewall settings and configuration itself. Our weapon of choice here is <a href=\"http://saltstack.com/\">Saltstack</a>. The added benefit of doing this now is that we won't ever need to dig through the servers to find out what our configuration looks like in the future.</p><p>I don’t have particularly strong feelings one way or the other when it comes to configuration management tools. It needs to get the job done and blend in with our overall philosophy of not being overloaded. Saltstack does that for us and it hasn’t let us down. The infrastructure repository contains about 150 salt state files and holds all the information needed (without passwords, of course) to set up <strong>Ghost(Pro)</strong>.</p><p>Although setting up Saltstack took some time, it's already shown its power. It took about a month to set up the first complete clone of the whole network. The second iteration only took one week. When I started to do the third installation of what would become our new hosted platform it only took me 2 days to have 20 servers deployed and ready.</p><p>Formalising our setup also surfaced a lot of small parts that need improvement which I added to our list of refactoring tasks.</p><p>Another thing we found to speed up managing our infrastructure was <a href=\"http://docs.saltstack.com/en/latest/topics/cloud/\">Salt-cloud</a>. It proved to be a really important tool for starting, stopping and deploying servers within minutes. It integrates closely with <a href=\"https://developers.digitalocean.com/\">DigitalOcean's API</a> and allows us to manage the infrastructure from the command line.</p>","image":"https://images.unsplash.com/photo-1504711988183-b5f6f0b1cb8a?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjExNzczfQ&s=2760b966a67ec69c138b60a5c450f6e7","tags":[{"name":"Inside Ghost","slug":"inside-ghost"},{"name":"#devblog","slug":"hash-dev-blog"},{"name":"#blog","slug":"hash-blog"}],"section":"blog","url":"https://ghost.org/blog/migrating-to-digitalocean/#inventory","headings":["Inventory"],"anchor":"inventory","customRanking":{"position":17,"heading":80},"objectID":"Ghost__Post__5cf5f94b3a5f1b0038cc70a1_1","_snippetResult":{"html":{"value":"started to do the third <ais-highlight-0000000000>installation</ais-highlight-0000000000> of what would become","matchLevel":"full"}},"_highlightResult":{"title":{"value":"How we migrated 30,000 blogs & 400,000 users across an ocean with zero downtime","matchLevel":"none","matchedWords":[]},"html":{"value":"<p>The first step was to create an inventory of what was currently running on our servers.</p><p>Every system administrator knows what software is running in their datacenter, but I don't know anyone who could replicate the configuration of multiple servers by heart. Including me. So we needed to gather all of the important (and maybe unimportant) information about our servers.</p><p>We introduced a configuration management system to keep track of installed software packages, firewall settings and configuration itself. Our weapon of choice here is <a href=\"http://saltstack.com/\">Saltstack</a>. The added benefit of doing this now is that we won't ever need to dig through the servers to find out what our configuration looks like in the future.</p><p>I don’t have particularly strong feelings one way or the other when it comes to configuration management tools. It needs to get the job done and blend in with our overall philosophy of not being overloaded. Saltstack does that for us and it hasn’t let us down. The infrastructure repository contains about 150 salt state files and holds all the information needed (without passwords, of course) to set up <strong>Ghost(Pro)</strong>.</p><p>Although setting up Saltstack took some time, it's already shown its power. It took about a month to set up the first complete clone of the whole network. The second iteration only took one week. When I started to do the third <ais-highlight-0000000000>installation</ais-highlight-0000000000> of what would become our new hosted platform it only took me 2 days to have 20 servers deployed and ready.</p><p>Formalising our setup also surfaced a lot of small parts that need improvement which I added to our list of refactoring tasks.</p><p>Another thing we found to speed up managing our infrastructure was <a href=\"http://docs.saltstack.com/en/latest/topics/cloud/\">Salt-cloud</a>. It proved to be a really important tool for starting, stopping and deploying servers within minutes. It integrates closely with <a href=\"https://developers.digitalocean.com/\">DigitalOcean's API</a> and allows us to manage the infrastructure from the command line.</p>","matchLevel":"full","fullyHighlighted":false,"matchedWords":["installation"]},"tags":[{"name":{"value":"Inside Ghost","matchLevel":"none","matchedWords":[]}},{"name":{"value":"#devblog","matchLevel":"none","matchedWords":[]}},{"name":{"value":"#blog","matchLevel":"none","matchedWords":[]}}],"url":{"value":"https://ghost.org/blog/migrating-to-digitalocean/#inventory","matchLevel":"none","matchedWords":[]},"headings":[{"value":"Inventory","matchLevel":"none","matchedWords":[]}]}}
+]
 
         console.log(hits)
         console.log(hits.length)
@@ -178,6 +181,10 @@ class Results extends React.Component {
 
         return (
             <>
+            {
+              // FIXME the following Autosuggest property does not work
+              // multiSection={true}
+            }
                 <Autosuggest
                     suggestions={hits}
                     onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
@@ -185,6 +192,9 @@ class Results extends React.Component {
                     getSuggestionValue={this.getSuggestionValue}
                     renderSuggestion={this.renderSuggestion}
                     inputProps={inputProps}
+                    theme={theme}
+                    renderSectionTitle={this.renderSectionTitle}
+                    getSectionSuggestions={this.getSectionSuggestions}
                 />
                 {
                 // <Index indexName="faq" />
