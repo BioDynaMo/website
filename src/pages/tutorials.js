a865bfe77badb47d0e2055cda76dbeb159bac5ee
banberina <berina.bandic@gmail.com>
2020-07-29 12:44:20 +0200

    Tutorials page displays all the generated html notebooks from static/notebooks

--- src/pages/tutorials.js
+++ src/pages/tutorials.js
@@ -2,63 +2,55 @@ import React from 'react'
 import { Link } from 'gatsby'
 import { StaticQuery, graphql } from 'gatsby'
 import PropTypes from 'prop-types'
-import { PostHeader, Icon, Box} from '../components/common'
+import { PostHeader, Icon, Box } from '../components/common'
 import { Layout } from '../components/common/layout'
 import { Spirit } from '../styles/spirit-styles'
 import { MetaData, getMetaImageUrls } from '../components/common/meta'
 import { TutorialBox } from '../components/tutorials'
 
-const Tutorials = ({data, location}) => {
+const Tutorials = ({ data, location }) => {
     console.log(data)
-	const title = `Tutorials`
-	const description = `This is the tutorials page.`
-	const imageUrl = getMetaImageUrls()
+    const title = `Tutorials`
+    const description = `This is the tutorials page.`
+    const imageUrl = getMetaImageUrls()
 
-	return(
+    return (
 		<>
 			<MetaData
-				data={data}
-				location={location}
-				title={title}
-				type="website"
-				description={description}
-				image={imageUrl}
+			    data={data}
+			    location={location}
+			    title={title}
+			    type="website"
+			    description={description}
+			    image={imageUrl}
 			/>
 			<Layout mainClass="bg-whitegrey-l2 pb-vw3" bodyClass="bg-white">
 
-                <div className="bg-concepts">
-                    <div className={`${Spirit.page.xl} pt12 pb4 pt-vw1-ns pb-vw1-ns white pl10 pl0-ns`}>
-                        <h1 className={`${Spirit.h4} gh-integration-header-shadow`}>
-                            <Link to="/tutorials/" className="link dim white">{title}</Link>
-                        </h1>
-                    </div>
-                </div>
+			    <div className="bg-concepts">
+			        <div className={`${Spirit.page.xl} pt12 pb4 pt-vw1-ns pb-vw1-ns white pl10 pl0-ns`}>
+			            <h1 className={`${Spirit.h4} gh-integration-header-shadow`}>
+			                <Link to="/tutorials/" className="link dim white">{title}</Link>
+			            </h1>
+			        </div>
+			    </div>
 
-                <div className={`${Spirit.page.xl} mt-vw3`}>
+			    <div className={`${Spirit.page.xl} mt-vw3`}>
 
-                    <section className="grid-12 gutter-row-20 gutter-20-ns gutter-36-l">
+			        <section className="grid-12 gutter-row-20 gutter-20-ns gutter-36-l">
 
-                        <TutorialBox to="/docs/userguide/" title="Demo 2" src="/images/paraview7-4.png">
-                            <p className={`${Spirit.p} mw70`}>
-                                Description here!
-                            </p>
-                        </TutorialBox>
+			            {
+			                data.notebooks.edges.map(edge => (
+                                <TutorialBox to={"/notebooks/"+edge.node.relativePath}  title={edge.node.name.charAt(0).toUpperCase()+edge.node.name.slice(1).replace("_", " ")} src="/images/paraview7-4.png">
+			                    </TutorialBox>
+			                ))
+			            }
+			        </section>
 
-                        <TutorialBox to="/docs/userguide/" title="Demo 3" src="/images/paraview7-4.png">
-                            <p className={`${Spirit.p} mw70`}>
-                                Description here!
-                            </p>
-                        </TutorialBox>
-
-                        
-
-                    </section>
-
-                </div>
+			    </div>
 
 			</Layout>
 		</>
-	)
+    )
 }
 
 Tutorials.propTypes = {
@@ -80,17 +72,16 @@ export default Tutorials
 
 export const tutorialsQuery = graphql`
     query {
-        siteMetaData: site {
+        site {
             ...SiteMetaFields
         }
         notebooks: allFile(filter: {sourceInstanceName: {eq: "notebooks"}}) {
             edges {
-              node {
-                name
-                relativePath
-              }
+                node {
+                    name
+                    relativePath
+                }
             }
-          }
+        }
     }
 `
-            
