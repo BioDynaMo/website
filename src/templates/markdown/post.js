088eda8f1af4192922565fd867b58421fd7a57fb
Muhammad Hassan Ahmed <33365802+hassan11196@users.noreply.github.com>
2021-07-28 18:11:05 +0500

    Remove extra tab

--- src/templates/markdown/post.js
+++ src/templates/markdown/post.js
@@ -56,12 +56,9 @@ class Post extends React.Component {
     render() {
         const { location } = this.props
         const post = this.props.data.markdownRemark
-      
         const githubLink = getGitHubLink(post.fileAbsolutePath)
-
         const imageUrl = getMetaImageUrls()
         const sideBarLayout = {}
-
         const { sidebar } = post.frontmatter || ``
         const toc = post.frontmatter.toc === false ? false : true
 
