e1796a4331f6cec77648ecf3a909997dc6cf57a2
Fons Rademakers <Fons.Rademakers@cern.ch>
2021-10-20 01:04:52 +0200

    Add code to enable Google Analytics for the site.

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,6 +32,24 @@ const plugins = [
      *  Content Plugins
      */
     {
+        resolve: `gatsby-plugin-google-gtag`,
+        options: {
+            // You can add multiple tracking ids and a pageview event will be fired for all of them.
+            trackingIds: [
+              "G-KZH62X093T", // Google Analytics / GA
+            ],
+            // This object is used for configuration specific to this plugin
+            pluginConfig: {
+                // Puts tracking script in the head instead of the body
+                head: true,
+                // Setting this parameter is also optional
+                //respectDNT: true,
+                // Avoids sending pageview hits from custom paths
+                //exclude: ["/preview/**", "/do-not-track/me/too/"],
+            },
+        },
+    },
+    {
         resolve: `gatsby-source-filesystem`,
         options: {
             path: path.join(__dirname, `content`),
