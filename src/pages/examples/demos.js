b2a330033b8983d7202e6ea109c29f24b64cad7d
Lukas Johannes Breitwieser <lukas.johannes.breitwieser@cern.ch>
2021-11-16 16:03:11 +0100

    Add gitpod links

--- src/pages/examples/demos.js
+++ src/pages/examples/demos.js
@@ -81,7 +81,7 @@ const Tutorials = ({ data, location }) => {
                                     html={"/notebooks/" + edge.node.relativePath}
                                     title={edge.node.relativePath.split("_").join(" ")}
                                     src={thumbnails[edge.node.relativePath] || '/images/default-demo-thumbnail.png'}
-                                    binder={"https://mybinder.org/v2/gh/BioDynaMo/binder-demo/master?urlpath=lab/tree/demo/" + edge.node.relativePath}>
+                                    link={"https://gitpod.io/#BDM_TRY_DEMO=" + edge.node.relativePath + "/https://github.com/LukasBreitwieser/gitpod-vnc"}>
                                 </TutorialBox>
                             ))
                         }
