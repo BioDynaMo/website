9c8db339754a4da0a3e36c1b7be73904ab1e0c3d
Fons Rademakers <Fons.Rademakers@cern.ch>
2022-03-10 19:01:14 +0100

    Update list of consortium members, add CBS and UNIGE IGH and remove Newcastle.

--- src/components/common/layout/Footer.js
+++ src/components/common/layout/Footer.js
@@ -12,8 +12,9 @@ const Footer = () => (
             <div className='consortium-footer-wrapper'>
                 <a className='consortium-logo cern-logo ' href="https://home.cern/" target="_blank"><img src="/images/consortium_logos/cern.png"/></a>
                 <a className='consortium-logo' href="https://openlab.cern/" target="_blank"><img src="/images/consortium_logos/cern-openlab.png"/></a>
-                <a className='consortium-logo' href="https://www.ncl.ac.uk/" target="_blank"><img src="/images/consortium_logos/newcastle.png"/></a>
                 <a className='consortium-logo' href="https://www.surrey.ac.uk/" target="_blank"><img src="/images/consortium_logos/surrey.jpg"/></a>
+                <a className='consortium-logo' href="https://www.unige.ch/medecine/isg/en/" target="_blank"><img src="/images/consortium_logos/unige-igh.jpg"/></a>
+                <a className='consortium-logo' href="https://www.cbs.nl/en-gb" target="_blank"><img src="/images/consortium_logos/cbs.jpg"/></a>
                 <a className='consortium-logo' href="https://www.gsi.de/en/start/news.htm" target="_blank"><img src="/images/consortium_logos/gsi.png"/></a>
                 <a className='consortium-logo' href="https://immunobrain.com/" target="_blank"><img src="/images/consortium_logos/ibc.png"/></a>
                 <a className='consortium-logo' href="https://www.ucy.ac.cy/en/" target="_blank"><img src="/images/consortium_logos/cyprus.png"/></a>
@@ -28,7 +29,7 @@ const Footer = () => (
                     <li className={`${listItemClass} dn db-l`}><a href="http://forum.biodynamo.org/" className={linkClass}>Forum</a></li>
                     <li className={`${listItemClass} dn db-l`}><a href="/blog/" className={linkClass}>Blogs</a></li>
                     <li className={`${listItemClass} dn db-l`}><a href="/docs/devguide/contribute/" className={linkClass}>Contribute</a></li>
-                    <li className={`${listItemClass} dn db-l`}><a href="/docs/userguide/contact/" className={linkClass}>Contact</a></li>                    
+                    <li className={`${listItemClass} dn db-l`}><a href="/docs/userguide/contact/" className={linkClass}>Contact</a></li>
                     <li className={`${listItemClass} dn db-l`}><a href="/team/" className={linkClass}>About Us</a></li>
                 </ul>
             </div>
